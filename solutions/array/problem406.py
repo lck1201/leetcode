34d626e07c7dfc74d525b8f04f14849d4cfe239a
lichuankang <lichuankang1201@gmail.com>
2019-01-23 23:34:14 +0800

    update

--- solutions/array/problem406.py
+++ solutions/array/problem406.py
@@ -2,7 +2,7 @@ from functools import cmp_to_key
 
 def mycmp(a, b):
     if a[0] == b[0]:
-        return -(a[1] - b[1])
+        return b[1] - a[1]
     return a[0] - b[0]
 
 class Solution:
---
+++ solutions/dp/problem494.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        def subsetSum(nums, target):
+            dp = [0] * (target+1)
+            dp[0] = 1
+            for n in nums:
+                for i in range(target, n-1,-1):
+                    dp[i] += dp[i-n]
+
+            return dp[target]
+
+
+        ns = sum(nums)
+        return 0 if ns<S or (S + ns)%2>0 else subsetSum(nums, (S+ns)//2)
+
+
+
+class Solution2(object):
+    def findTargetSumWays(self, nums, S):
+        if not nums:
+            return 0
+        dic = {nums[0]: 1, -nums[0]: 1} if nums[0] != 0 else {0: 2}
+        for i in range(1, len(nums)):
+            tdic = {}
+            for d in dic:
+                tdic[d + nums[i]] = tdic.get(d + nums[i], 0) + dic.get(d, 0)
+                tdic[d - nums[i]] = tdic.get(d - nums[i], 0) + dic.get(d, 0)
+            dic = tdic
+        return dic.get(S, 0)
